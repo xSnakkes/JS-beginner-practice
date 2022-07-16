b43c1d249a0769b212619f22faad1885fbde1d1a
xSnakkes <102847435+xSnakkes@users.noreply.github.com>
2022-07-16 22:26:31 +0300

    change filename

--- pracrice5.js
+++
@@ -1,9 +0,0 @@
-let firstNumber = Number(prompt('Введіть перше число:'));
-let secondNumber = Number(prompt('Введіть друге число:'));
-
-let choseNum = (x,y) => {
-    (x > y) ? result = (x+y):
-    (x < y) ? result = (x*y):
-    result = (x, y); return result}
-
-alert(choseNum(firstNumber, secondNumber));
